19be8919bfc4772e31b7a7ca7801914c666d14d4
Alan Scarpa <alan.scarpa@gmail.com>
2016-03-26 18:28:38 -0400

    First commit

---
+++ Imprint/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Imprint
+//
+//  Created by Alan Scarpa on 3/26/16.
+//  Copyright © 2016 Imprint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
