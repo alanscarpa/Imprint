48d7739528a1ef2022558393d6dbcfa083142c20
Alan Scarpa <alan.scarpa@gmail.com>
2016-03-26 20:12:37 -0400

    Added header

--- Imprint/ViewController.swift
+++ Imprint/ViewController.swift
@@ -12,6 +12,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let titleImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+        titleImage.image = UIImage(named: "imprintHeader")
+        titleImage.contentMode = .ScaleAspectFit
+        self.navigationItem.titleView = titleImage
+//        viewController.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"yourimage.png"]];
+//        UIBarButtonItem * item = [[UIBarButtonItem alloc] initWithCustomView:[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"yourimage2.jpg"]]];
+//        viewController.navigationItem.rightBarButtonItem = item;
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
